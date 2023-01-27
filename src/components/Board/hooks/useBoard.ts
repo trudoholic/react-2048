31dd140ca8fead863e7c53590add4524e7f02b98
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-01-28 00:15:49 +0300

    fix

--- src/components/Board/hooks/useBoard.ts
+++ src/components/Board/hooks/useBoard.ts
@@ -5,8 +5,7 @@ import { BoardContext } from "../context/BoardContext";
  * Returns the properties of the board such as tile container width or tile count.
  */
 export const useBoard = () => {
-    const { containerWidth, tileCount } = { containerWidth: 0, tileCount: 4 };
-    // const { containerWidth, tileCount } = useContext(BoardContext);
+    const { containerWidth, tileCount } = useContext(BoardContext);
 
     return [containerWidth, tileCount] as [number, number];
 };
